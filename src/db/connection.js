b02a4632e3dc24080a6b1e0af5525229e1b069ee
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-04-01 23:25:06 -0300

    atualizado

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 export const connection = new Sequelize(
-    DB_BASE = jogos,
-    DB_USER = postgres,
-    DB_PASS = Pa44528033,
+    process.env.DB_BASE,
+    process.env.DB_USER,
+    process.env.DB_PASS,
     {
         dialect: 'postgres',
         dialectOptions: {
